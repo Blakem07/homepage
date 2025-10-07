98500727e40017a6620ccfa9bed9d297526a6845
Blakem07 <Blakem2020@gmail.com>
2025-10-07 07:08:25 +0100

    feat: implemented webpack template so code can be bundled later for deployment

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import "./styles.css";
+import "./css-reset.css";
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(mp4|webm|ogg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
