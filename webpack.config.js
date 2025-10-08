f1f33326734ad3e09818a8fb6c9f450d0b15f6a0
Blakem07 <Blakem2020@gmail.com>
2025-10-08 11:11:42 +0100

    --amend

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/homepage/",
+    filename: "main.js",
+    clean: true,
   },
   devtool: "eval-source-map",
   devServer: {
